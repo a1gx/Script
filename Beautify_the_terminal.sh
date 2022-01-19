eac259131ff1aa3e0d6b25a014d0400dfce96c79
A1GX <49387206+a1gx@users.noreply.github.com>
2022-01-19 18:54:12 +0800

    Create Beautify_the_terminal.sh
    专属于ubuntu18的终端美化脚本

---
+++ Beautify_the_terminal.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt update
+apt install zsh -y
+apt install tmux -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+apt install autojump -y
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
